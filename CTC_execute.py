48a789a1470b190f4ad59414d8f3edcce81850ae
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-09-01 09:34:41 -0300

    primeiro rascunho de cada parte

---
+++ CTC_execute.py
@@ -0,0 +1,13 @@
+
+# * IMPORTS - modules
+from C_T_C.src.backEnd.Main import main
+
+# * IMPORTS - libraries
+...
+
+class CTC_execute:
+    def run(self):
+        print("CTC_execute.py is running...")
+        main()
+
+if __name__ == "__main__": CTC_execute().run()
---
+++ C_T_C/src/frontEnd/Main_menu.py
@@ -0,0 +1,13 @@
+
+# * IMPORTS - modules
+...
+
+# * IMPORTS - libraries
+...
+
+class MainMenu:
+    def __init__(self, screen_size:tuple[int, int]):
+        self.screen_size = screen_size
+        print("Menu.py initialized with ScreenManager.")
+    def display_menu(self):
+        print("Displaying menu...")
